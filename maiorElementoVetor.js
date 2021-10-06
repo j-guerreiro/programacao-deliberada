272e2f1a964cdb28cec1b785a97d4ce2b0b3e47e
jguerreiro <joel.borghi@gmail.com>
2021-10-06 19:03:30 -0300

    primeiro commit

---
+++ maiorElementoVetor.js
@@ -0,0 +1,20 @@
+/*
+    Temos um vetor de N inteiros;
+    Devemos encontrar dentro do vetor o maior valor e mostrar na tela;
+*/
+
+const vetor = [9, 10, 13, 12, 17, 20, 2];
+
+/* 
+   funcao recebe um vetor de inteiros e
+   encontra o maior valor
+*/
+function maiorElemento(vetor){
+    var maiorValor = vetor[0];
+    for(let i = 0; i < vetor.length; i++) 
+        if(vetor[i] > maiorValor)
+            maiorValor = vetor[i];
+    
+    return maiorValor;
+};
+console.log(maiorElemento(vetor));
